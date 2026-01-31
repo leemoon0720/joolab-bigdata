ee34b14a8c87cd4cc56542685c029ee539ba9a1d
leemoon0720 <2zshot0720@gmail.com>
2026-02-01 05:47:10 +0900

    Add files via upload

---
+++ functions/api/members/list.js
@@ -0,0 +1,11 @@
+export async function onRequest({ env }) {
+  const db = env.DB;
+
+  const { results } = await db
+    .prepare("SELECT * FROM members ORDER BY id DESC")
+    .all();
+
+  return new Response(JSON.stringify({ ok: true, items: results }), {
+    headers: { "Content-Type": "application/json" },
+  });
+}
