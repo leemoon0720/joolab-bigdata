94127e8f65bdf27a0a182bfe86e7294cc8d18219
leemoon0720 <2zshot0720@gmail.com>
2026-02-01 15:00:53 +0900

    Delete joolab-bigdata-main directory

--- joolab-bigdata-main/functions/whoami.js
+++
@@ -1,7 +0,0 @@
-export async function onRequest(context) {
-  const cookie = context.request.headers.get("Cookie") || "";
-  const admin = cookie.includes("admin=1");
-  return new Response(JSON.stringify({ admin }), {
-    headers: { "Content-Type": "application/json" }
-  });
-}
